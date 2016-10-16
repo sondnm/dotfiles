b24e218abf6faf927bb72e057bbab84a8046bc12
Son Dang <sondangdev@gmail.com>
2016-10-17 01:08:07 +0700

    Follows google shell styleguide for touchpad sh

--- sh/touchpad-toggle.sh
+++ sh/touchpad-toggle.sh
@@ -5,10 +5,10 @@
 
 #!/bin/bash
 declare -i ID
-ID=`xinput list | grep -Eio '(touchpad|glidepoint)\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
+ID=$(xinput list | grep -Eio '(touchpad|glidepoint)\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}')
 
 declare -i STATE
-STATE=`xinput list-props $ID | grep 'Device Enabled' | awk '{print $4}'`
+STATE=$(xinput list-props $ID | grep 'Device Enabled' | awk '{print $4}')
 
 if [ $STATE -eq 1 ]
 then
