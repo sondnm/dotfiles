f6104ef26d4cfeb4e17096e468b5186d184e0886
Son Dang <sondangdev@gmail.com>
2019-01-11 00:03:35 +0700

    Add English as default language to avoid vietnamese in git

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,4 @@ source $HOME/.cargo/env
 source $HOME/.config/fish/functions/__fit.fish
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -g fish_user_paths "/usr/local/mysql/bin" $fish_user_paths
+set -x LANG en_US.UTF-8
